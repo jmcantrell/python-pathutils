5c4b74cbb14dd22fab1d0396c1362c0a2cbfef84
Jeremy Cantrell <jmcantrell@gmail.com>
2009-02-26 21:47:15 -0500

    Adding existing files

---
+++ pathutils/tagging.py
@@ -0,0 +1,103 @@
+import os.path, hashlib
+
+from . import relative, expand, add_sep
+
+def sum_filename(filename):
+    m = hashlib.md5()
+    m.update(filename)
+    ext = os.path.splitext(filename)[1]
+    return m.hexdigest() + ext
+
+class PathTags(object):
+
+    def __init__(self, directory):
+        self.directory = expand(directory)
+        self.load()
+
+    def _get_basenames(self, directory):
+        names = {}
+        for root, dirs, files in os.walk(directory):
+            if root == self.directory: continue
+            names.update(dict(((f, os.path.join(root, f)) for f in files)))
+        return names
+
+    def get_tags(self, filename=None):
+        if filename: return self.tags.get(expand(filename), [])
+        return sorted(set(sum(self.tags.values(), [])))
+
+    def set_tags(self, filename, tags):
+        fn = expand(filename)
+        if tags:
+            self.tags[fn] = sorted(set(tags))
+        elif fn in self.tags:
+            del(self.tags[fn])
+
+    def add_tags(self, filename, tags):
+        fn = expand(filename)
+        self.set_tags(fn, self.get_tags(fn) + tags)
+
+    def remove_tags(self, filename, tags):
+        fn = expand(filename)
+        self.set_tags(fn, list(set(self.get_tags(fn)) - set(tags)))
+
+    def get_paths(self, tag=None):
+        return sorted([fn for fn, t in self.tags.items() if not tag or tag in t])
+
+
+    def repair(self, directory):
+        pool = self._get_basenames(directory)
+        delete = []
+        update = {}
+        for path, tags in self.tags.iteritems():
+            if os.path.exists(path): continue
+            fn = os.path.basename(path)
+            delete.append(path)
+            if fn not in pool: continue
+            update[pool[fn]] = tags
+        for path in delete:
+            del(self.tags[path])
+        self.tags.update(update)
+
+    def write(self):
+        # Remove items that have no tags
+        self.tags = dict((k, v) for k, v in self.tags.iteritems() if v)
+        for fn, tags in self.old_tags.iteritems():
+            sumfn = sum_filename(fn)
+            if fn in self.tags and self.tags[fn]: continue
+            for tag in tags:
+                d = os.path.join(self.directory, tag)
+                if not os.path.isdir(d): continue
+                link = os.path.join(d, sumfn)
+                if os.path.islink(link):
+                    os.remove(link)
+        for fn, tags in self.tags.iteritems():
+            sumfn = sum_filename(fn)
+            # Get rid of the tags that no longer exist
+            for tag in set(self.old_tags.get(fn, [])) - set(tags):
+                d = os.path.join(self.directory, tag)
+                if not os.path.isdir(d): continue
+                link = os.path.join(d, sumfn)
+                if os.path.islink(link):
+                    os.remove(link)
+            # Store the current tags
+            for tag in tags:
+                d = os.path.join(self.directory, tag)
+                if not os.path.isdir(d): os.makedirs(d)
+                link = os.path.join(d, sumfn)
+                if os.path.exists(fn) and not os.path.exists(link):
+                    os.symlink(relative(add_sep(d), fn), link)
+        self.old_tags = self.tags.copy()
+
+    def load(self):
+        self.tags = {}
+        self.old_tags = {}
+        if not os.path.isdir(self.directory): return
+        for tag in os.listdir(self.directory):
+            directory = os.path.join(self.directory, tag)
+            if not os.path.isdir(directory): continue
+            for sumfn in os.listdir(directory):
+                fn = os.path.join(directory, sumfn)
+                if not os.path.islink(fn): continue
+                fn = os.path.normpath(os.path.join(directory, os.readlink(fn)))
+                self.add_tags(fn, [tag])
+        self.old_tags.update(self.tags)
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from glob import glob
+
+setup(
+        name='PathUtils',
+        version='0.6',
+        description='Various small utilities for working with paths.',
+        author='Jeremy Cantrell',
+        author_email='jmcantrell@gmail.com',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Natural Language :: English',
+            'Programming Language :: Python',
+            ],
+        packages=[
+            'pathutils',
+            ],
+        )
